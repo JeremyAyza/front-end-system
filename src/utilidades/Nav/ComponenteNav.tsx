3c88e4af3ba66744ed2c5c1c7733efa2a5abc894
JeremyAyza <Ayzajeremyx7x@gmail.com>
2023-01-20 20:07:33 -0500

    fix routes

--- src/utilidades/Nav/ComponenteNav.tsx
+++ src/utilidades/Nav/ComponenteNav.tsx
@@ -8,7 +8,7 @@ export default function ComponenteNav() {
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span> 
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse" id="navbarNav">q
           <ul className="navbar-nav">
             <li className="nav-item">
               <Link className="nav-link" to={"/usuario/listar"}>Usuarios</Link>
